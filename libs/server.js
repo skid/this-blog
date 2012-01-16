c574eeca5de0b839456796c8c4d9bfbdf428d85a
Dusko Jordanovski <jordanovskid@gmail.com>
2012-01-16 17:51:23 +0100

    Added memory caching based on URLs

--- libs/server.js
+++ libs/server.js
@@ -244,6 +244,7 @@ main.use('/', function(err, req, res, next){
     title:   err.status === 404 ? "Not Found" : "Internal Server Error"
   }
   utils.template(path.normalize(__dirname + "/../templates/error.html"), req.context, function(error, html){
+    console.log(err)
     res.writeHead(err.status || 500);
     res.end(html || "500 Internal Server Error");
   });
